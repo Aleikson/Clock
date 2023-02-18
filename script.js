4ac8cc81d2a8d40b6e7a99c1e80b47bdfb59eddc
aleikson <aleiksonsilva@hotmail.com>
2023-02-18 10:36:37 -0300

    bug fixed

--- script.js
+++ script.js
@@ -6,18 +6,35 @@ const hourHand = document.querySelector('.hour-hand');
 function setDate() {
     const now = new Date();
 
-    const seconds = now.getSeconds();
+    let seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    const mins = now.getMinutes();
+    let mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + 90;
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-    const hours = now.getHours();
+    let hours = now.getHours();
     const hoursDegrees = ((hours / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
-    document.getElementById("digital-clock").innerText = hours + " : " + mins + " : " + seconds;
+    let period = "AM";
+
+    if (hours > 12) {
+        hours-= 12;
+        period = "PM";
+    }
+
+    if (hours === 0) {
+        hours = 12;
+        period = "AM";
+    }
+
+    hours = hours < 10 ? "0" + hours : hours;
+    mins = mins < 10 ? "0" + mins : mins;    
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    document.getElementById("digital-clock").innerText = hours + " : " + mins + " : " + seconds + " " + period;
+
 }
 setInterval(setDate, 1000);
